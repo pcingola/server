7bf806f568b78a7bbd8419aaa71f285a563ff562
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-09-04 19:02:59 -0400

    Adding 404 check to testGetDataset

--- tests/unit/test_views.py
+++ tests/unit/test_views.py
@@ -299,12 +299,19 @@ class TestFrontend(unittest.TestCase):
         self.assertEqual(len(responseData.variantSets), 1)
 
     def testGetDataset(self):
+        # Test OK: ID found
         response = self.sendDatasetsSearch()
         responseData = protocol.SearchDatasetsResponse.fromJsonString(
             response.data)
         datasetId = responseData.datasets[0].id
         response = self.sendGetDataset(datasetId)
         self.assertEqual(200, response.status_code)
+
+        # Test Error: 404, ID not found
+        obfuscated = datamodel.CompoundId.obfuscate("notValid")
+        compoundId = datamodel.DatasetCompoundId.parse(obfuscated)
+        response = self.sendGetDataset(str(compoundId))
+        self.assertEqual(404, response.status_code)
 
     def testGetVariantSet(self):
         response = self.sendVariantSetsSearch()
@@ -369,23 +376,6 @@ class TestFrontend(unittest.TestCase):
             response.data)
         datasets = list(responseData.datasets)
         self.assertEqual(self.datasetId, datasets[0].id)
-
-    # def testNotImplementedPaths(self):
-    #     pathsNotImplementedPost = [
-    #     ]
-    #     pathsNotImplementedGet = [
-    #     ]
-    #
-    #     def runRequest(method, path):
-    #         requestPath = path.replace('<id>', 'someId')
-    #         response = method(requestPath)
-    #         protocol.GAException.fromJsonString(response.get_data())
-    #         self.assertEqual(response.status_code, 501)
-    #
-    #     for path in pathsNotImplementedGet:
-    #         runRequest(self.app.get, path)
-    #     for path in pathsNotImplementedPost:
-    #         runRequest(self.app.post, path)
 
     def testNoAuthentication(self):
         path = '/oauth2callback'
